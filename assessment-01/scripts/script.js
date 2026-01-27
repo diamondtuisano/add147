a7d32406ce3eb8cd924c6f57be10814bf9f61f44
Diamond Tuisano <dtuisano@students.mchenry.edu>
2026-01-27 17:18:36 -0600

    assessment01

---
+++ assessment-01/scripts/script.js
@@ -0,0 +1,18 @@
+function myFunction2() {
+    document.getElementById("diamond").innerHTML =
+        "Changed from a function in the External Script.";
+}
+
+function myFunction3() {
+    document.getElementById('food').style.width = "800px";
+
+}
+
+function myFunction4() {
+    document.getElementById("diamond").style.color = "rgba(159, 99, 31, 1)";
+}
+// single line comment
+
+/*
+block comment
+*/
